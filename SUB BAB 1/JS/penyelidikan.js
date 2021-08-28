7a5fc31e3090e38f7daf091b775a48b7bb1bfd0e
herdiii <herdiaulia14@gmail.com>
2021-08-28 08:13:10 +0800

    perbaikan

--- SUB BAB 1/JS/penyelidikan.js
+++ SUB BAB 1/JS/penyelidikan.js
@@ -1,3 +1,14 @@
+/*
+*******************
+*** SETUP VIDEO ***
+*******************
+*/
+const videoPenyelidikan = document.getElementById("videoPenyelidikan");
+
+videoPenyelidikan.onended = function() {
+  document.getElementById("qw").classList.add("hide");
+  document.getElementById("we").classList.remove("hide");
+};
 
 /*
 *************************************
