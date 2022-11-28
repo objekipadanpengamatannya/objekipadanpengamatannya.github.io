645cc614013901dc0bc3b1e48ae60a6e7aabf34e
herdiii <herdiaulia14@gmail.com>
2022-11-28 15:22:38 +0800

    281022

--- SUB BAB 1/JS/objek2.js
+++ SUB BAB 1/JS/objek2.js
@@ -1,8 +1,112 @@
 /*
-**********************************
-***** SETUP BUTTON OBJEK *****
-**********************************
-*/
+ **********************************
+ ***** SETUP SOAL OBJEK 1 *********
+ **********************************
+ */
+
+// NO 1
+
+const btnSatuA = document.getElementById("btnSatuA");
+const btnSatuB = document.getElementById("btnSatuB");
+const btnSatuC = document.getElementById("btnSatuC");
+const btnSatuD = document.getElementById("btnSatuD");
+
+btnSatuA.addEventListener('click', function () {
+  document.getElementById("benarSatu").classList.remove("hide");
+  // document.getElementById("soalDua").classList.remove("hide");
+  btnSatuA.style.background = "green";
+
+  btnSatuA.disabled = true;
+  btnSatuB.disabled = true;
+  btnSatuC.disabled = true;
+  btnSatuD.disabled = true;
+})
+btnSatuB.addEventListener('click', function () {
+  document.getElementById("salahSatu").classList.remove("hide");
+  // document.getElementById("soalDua").classList.remove("hide");
+  btnSatuB.style.background = "red";
+
+  btnSatuA.disabled = true;
+  btnSatuB.disabled = true;
+  btnSatuC.disabled = true;
+  btnSatuD.disabled = true;
+})
+btnSatuC.addEventListener('click', function () {
+  document.getElementById("salahSatu").classList.remove("hide");
+  // document.getElementById("soalDua").classList.remove("hide");
+  btnSatuC.style.background = "red";
+
+  btnSatuA.disabled = true;
+  btnSatuB.disabled = true;
+  btnSatuC.disabled = true;
+  btnSatuD.disabled = true;
+})
+btnSatuD.addEventListener('click', function () {
+  document.getElementById("salahSatu").classList.remove("hide");
+  // document.getElementById("soalDua").classList.remove("hide");
+  btnSatuD.style.background = "red";
+
+  btnSatuA.disabled = true;
+  btnSatuB.disabled = true;
+  btnSatuC.disabled = true;
+  btnSatuD.disabled = true;
+})
+
+// NO 2
+
+const btnDuaA = document.getElementById("btnDuaA");
+const btnDuaB = document.getElementById("btnDuaB");
+const btnduaC = document.getElementById("btnDuaC");
+const btnDuaD = document.getElementById("btnDuaD");
+
+btnDuaA.addEventListener('click', function () {
+  document.getElementById("salahDua").classList.remove("hide");
+  // document.getElementById("soalTiga").classList.remove("hide");
+  btnDuaA.style.background = "green";
+
+  btnDuaA.disabled = true;
+  btnDuaB.disabled = true;
+  btnDuaC.disabled = true;
+  btnDuaD.disabled = true;
+})
+btnDuaB.addEventListener('click', function () {
+  document.getElementById("salahDua").classList.remove("hide");
+  // document.getElementById("soalTiga").classList.remove("hide");
+  btnDuaB.style.background = "red";
+
+  btnDuaA.disabled = true;
+  btnDuaB.disabled = true;
+  btnDuaC.disabled = true;
+  btnDuaD.disabled = true;
+})
+btnDuaC.addEventListener('click', function () {
+  document.getElementById("salahDua").classList.remove("hide");
+  // document.getElementById("soalTiga").classList.remove("hide");
+  btnDuaC.style.background = "red";
+
+  btnDuaA.disabled = true;
+  btnDuaB.disabled = true;
+  btnDuaC.disabled = true;
+  btnDuaD.disabled = true;
+})
+btnDuaD.addEventListener('click', function () {
+  document.getElementById("benarDua").classList.remove("hide");
+  // document.getElementById("soalTiga").classList.remove("hide");
+  btnDuaD.style.background = "green";
+
+  btnDuaA.disabled = true;
+  btnDuaB.disabled = true;
+  btnDuaC.disabled = true;
+  btnDuaD.disabled = true;
+})
+
+/*
+ **********************************
+ ***** SETUP BUTTON OBJEK *********
+ **********************************
+ */
+
+// Objek Biotik dan Abiotik
 
 const atom = document.querySelector('#atom');
 const virus = document.querySelector('#virus');
@@ -18,7 +122,7 @@ const btnBumi = document.querySelector('#btnBumi');
 const btnMatahari = document.querySelector('#btnMatahari');
 const btnLautan = document.querySelector('#btnLautan');
 
-btnAtom.addEventListener("click", function(){
+btnAtom.addEventListener("click", function () {
   atom.classList.remove('hide');
   virus.classList.add('hide');
   bakteri.classList.add('hide');
@@ -34,7 +138,7 @@ btnAtom.addEventListener("click", function(){
   btnLautan.classList.remove('btn-bgpeach-aktif');
 })
 
-btnVirus.addEventListener("click", function(){
+btnVirus.addEventListener("click", function () {
   virus.classList.remove('hide');
   atom.classList.add('hide');
   bakteri.classList.add('hide');
@@ -50,7 +154,7 @@ btnVirus.addEventListener("click", function(){
   btnLautan.classList.remove('btn-bgpeach-aktif');
 })
 
-btnBakteri.addEventListener("click", function(){
+btnBakteri.addEventListener("click", function () {
   bakteri.classList.remove('hide');
   atom.classList.add('hide');
   virus.classList.add('hide');
@@ -66,7 +170,7 @@ btnBakteri.addEventListener("click", function(){
   btnLautan.classList.remove('btn-bgpeach-aktif');
 })
 
-btnBumi.addEventListener("click", function(){
+btnBumi.addEventListener("click", function () {
   bumi.classList.remove('hide');
   atom.classList.add('hide');
   virus.classList.add('hide');
@@ -82,7 +186,7 @@ btnBumi.addEventListener("click", function(){
   btnLautan.classList.remove('btn-bgpeach-aktif');
 })
 
-btnMatahari.addEventListener("click", function(){
+btnMatahari.addEventListener("click", function () {
   matahari.classList.remove('hide');
   atom.classList.add('hide');
   virus.classList.add('hide');
@@ -99,7 +203,7 @@ btnMatahari.addEventListener("click", function(){
   btnLautan.classList.remove('btn-bgpeach-aktif');
 })
 
-btnLautan.addEventListener("click", function(){
+btnLautan.addEventListener("click", function () {
   lautan.classList.remove('hide');
   atom.classList.add('hide');
   virus.classList.add('hide');
