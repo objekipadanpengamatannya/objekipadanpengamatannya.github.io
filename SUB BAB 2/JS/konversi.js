4ff513d342d0ad0fa67d49d7aedb0cc1ecf524dd
herdiii <herdiaulia14@gmail.com>
2023-01-15 06:44:30 +0800

    15123

--- SUB BAB 2/JS/konversi.js
+++ SUB BAB 2/JS/konversi.js
@@ -4,24 +4,85 @@
  ********************************
  */
 
-//  Setup contoh 1
+//  Setup contoh Panjang
 
 let klikDikPan = document.getElementById("klikDikPan");
 let klikDitPan = document.getElementById("klikDitPan");
 let klikjawPan = document.getElementById("klikJawPan");
 
-let ditPan = document.getElementById("ditPan");
 let dikPan = document.getElementById("dikPan");
+let ditPan = document.getElementById("ditPan");
 let jawPan = document.getElementById("jawPan");
 
 klikDikPan.addEventListener('click', function () {
-    ditPan.classList.remove("hide");
     klikDikPan.classList.add("hide");
+    dikPan.classList.remove("hide");
+    klikDitPan.classList.remove("hide");
 })
 
 klikDitPan.addEventListener('click', function () {
-    jawPan.classList.remove("hide");
     klikDitPan.classList.add("hide");
+    ditPan.classList.remove("hide");
+    klikJawPan.classList.remove("hide");
+})
+
+klikJawPan.addEventListener('click', function () {
+    klikJawPan.classList.add("hide");
+    jawPan.classList.remove("hide");
+})
+
+//  Setup contoh Massa
+
+let klikDikMas = document.getElementById("klikDikMas");
+let klikDitMas = document.getElementById("klikDitMas");
+let klikjawMas = document.getElementById("klikJawMas");
+
+let dikMas = document.getElementById("dikMas");
+let ditMas = document.getElementById("ditMas");
+let jawMas = document.getElementById("jawMas");
+
+klikDikMas.addEventListener('click', function () {
+    klikDikMas.classList.add("hide");
+    dikMas.classList.remove("hide");
+    klikDitMas.classList.remove("hide");
+})
+
+klikDitMas.addEventListener('click', function () {
+    klikDitMas.classList.add("hide");
+    ditMas.classList.remove("hide");
+    klikJawMas.classList.remove("hide");
+})
+
+klikJawMas.addEventListener('click', function () {
+    klikJawMas.classList.add("hide");
+    jawMas.classList.remove("hide");
+})
+
+//  Setup contoh Waktu
+
+let klikDikWak = document.getElementById("klikDikWak");
+let klikDitWak = document.getElementById("klikDitWak");
+let klikjawWak = document.getElementById("klikJawWak");
+
+let dikWak = document.getElementById("dikWak");
+let ditWak = document.getElementById("ditWak");
+let jawWak = document.getElementById("jawWak");
+
+klikDikWak.addEventListener('click', function () {
+    klikDikWak.classList.add("hide");
+    dikWak.classList.remove("hide");
+    klikDitWak.classList.remove("hide");
+})
+
+klikDitWak.addEventListener('click', function () {
+    klikDitWak.classList.add("hide");
+    ditWak.classList.remove("hide");
+    klikJawWak.classList.remove("hide");
+})
+
+klikJawWak.addEventListener('click', function () {
+    klikJawWak.classList.add("hide");
+    jawWak.classList.remove("hide");
 })
 
 /*
@@ -35,19 +96,19 @@ function cekKonversiPanjang() {
     var convP2 = document.getElementById("convP2").value;
     var convP3 = document.getElementById("convP3").value;
 
-    if (convP1 == 10) {
+    if (convP1 == 70) {
         document.getElementById("convP1").style = "border: 3px solid green";
     } else {
         document.getElementById("convP1").style = "border: 3px solid red";
     }
 
-    if (convP2 == 100) {
+    if (convP2 == 700) {
         document.getElementById("convP2").style = "border: 3px solid green";
     } else {
         document.getElementById("convP2").style = "border: 3px solid red";
     }
 
-    if (convP3 == 1000) {
+    if (convP3 == 7000) {
         document.getElementById("convP3").style = "border: 3px solid green";
     } else {
         document.getElementById("convP3").style = "border: 3px solid red";
